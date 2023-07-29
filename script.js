0f0e5e3425c4292daef3d15f0efa7715644cc071
Arpit jain <arpit2345809@gmail.com>
2023-07-29 13:17:54 +0530

    minor changes

--- script.js
+++ script.js
@@ -18,12 +18,15 @@ let songs = [
     { songname: "Tu mileya", filepath: "songs/6.mp3", coverpath: "covers/6.jpg" },
 
 ]
-songitems.forEach((element , i)=>{
-    element.getElementsByTagName('img')[0].src = songs[i].coverpath;
-    element.getElementsByClassName('songname')[0].innerText = songs[i].songname;
+9
+// yeh mne html se le li  idhar se bhi le skte h 
+// songitems.forEach((element , i)=>{
+//      element.getElementsByTagName('img')[0].src = songs[i].coverpath;
+//      element.getElementsByClassName('songname')[0].innerText = songs[i].songname;
+// })
 
-})
 
+// mtlb koi bhi audio play vaale pae click kre toh yeh ho
 masterplay.addEventListener('click', ()=>{
     /*ya toh audio element paused h || ya audio element start hi nhi  hua h*/
     // console.log('chal raha hai');
@@ -43,33 +46,38 @@ masterplay.addEventListener('click', ()=>{
     }
 })
 audioelement.addEventListener('timeupdate', ()=>{
-    // console.log('timeupdate');
     progress = parseInt((audioelement.currentTime / audioelement.duration) * 100) /*  P=(CT/DU)X100  */
     myprogressbar.value = progress;
 
 })
+
+// mtlb koi bhi gaane ko aage peeche change kre toh yeh ho
 myprogressbar.addEventListener('change', ()=>{
-    // console.log("chl raha hai");
     audioelement.currentTime = myprogressbar.value * audioelement.duration / 100; /*  CT=P*DU/100     */
 
 })
-const makeAllPlays = () => {
 
+// upar vaale play krne ke option h unko play kr dega yeh makeAllPlays ka yehi role hai yaha pr
+const makeAllPlays = () => {
     Array.from(document.getElementsByClassName('songitemplay')).forEach((element) => {
         element.classList.remove('fa-pause-circle');
         element.classList.add('fa-play-circle');
     })
+//     Array.from(document.getElementsByClassName('songitemplay')).forEach((element) => {
+//         element.classList.remove('fa-play-circle');
+//         element.classList.add('fa-pause-circle');
+// })
 }
 
-
+// mtlb play krne vaale button h unpe click krte hi play ho jae uske liye apn ne makeallplays function bnaya h
 Array.from(document.getElementsByClassName('songitemplay')).forEach((element) => {
     element.addEventListener('click', (e)=>{
-        console.log(e.target);
+        console.log(e.target);//e.target se vo element mil jaega jisse apn ne play kra h
         makeAllPlays();
         songindex = parseInt(e.target.id);
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        audioelement.src = `songs/${songindex+1}.mp3`;
+        audioelement.src = `songs/${songindex+1}.mp3`;//isse yeh pta chlega ki konsa gaana chalu kra h apn ne
         masterSongName.innerText = songs[songindex].songname;
         audioelement.currentTime = 0;
         audioelement.play();
@@ -108,7 +116,7 @@ document.getElementById('previous').addEventListener('click', () => {
         songindex -= 1;
     }
     audioelement.src = `songs/${songindex+1}.mp3`;
-    masterSongName.innerText = songs[songindex].songname;
+    masterSongName.innerText = songs[songindex].songname;//mtlb apn ne kabira song chalaye toh kabira hi likha aaye neeche uske aage vaala ya peeche vaala nhi likha aaye neeche
     audioelement.currentTime = 0;
     audioelement.play();
     gif.style.opacity = 1;
